5c88be9c4bf37e94353187bd0d8b6e20ca19d8dc
C Plus Plus Software Engineer <cppswengr@eVantageGroup.com>
2019-01-30 08:41:08 -0600

    Ch4, upto but not including Jasmine clock

--- ch4/spec/functionAdd.js
+++ ch4/spec/functionAdd.js
@@ -1,10 +1,10 @@
-describe("Currency Conversion Test Suite", function() {
+describe("Testing Mathematic operations", function() {
   // variables available for all specs
   var numberA = 3;
   var numberB = 2;
 
   // the function 'it' defines a spec
-  it("Testing the add() function", function() {
+  it("Spec for Add operation", function() {
     expect(add(numberA, numberB)).toEqual(5);
   });
 
--- ch4/spec/spyEmployee.js
+++ ch4/spec/spyEmployee.js
@@ -72,4 +72,50 @@ describe("Jasmine Spy", function() {
 
   });
 
+  it("Tracking spies with calls property",function(){
+    var alice = new Employee("Alice", 4, "Testing");
+
+    spyOn(alice,"calculateSalary").and.callThrough();
+    var salary = alice.getSalary(); // calls calculateSalary
+
+    alice.calculateSalary.and.stub();
+
+    // calls calculateSalary only if salary is zero
+    salary = alice.getSalary();
+
+    expect(salary).toEqual(4000);
+
+    expect(alice.calculateSalary.calls.any()).toEqual(true);
+    expect(alice.calculateSalary.calls.count()).toEqual(1);
+
+    // returns blank []
+    console.log(alice.calculateSalary.calls.argsFor(0));
+
+    expect(alice.calculateSalary.calls.argsFor(0)).toEqual([4]);
+
+    alice.calculateSalary(1000);
+
+    // returns array [1000]
+    console.log(alice.calculateSalary.calls.argsFor(1));
+
+
+    expect(alice.calculateSalary.calls.argsFor(1)).toEqual([1000]);
+
+    // returns [[], [1000]]
+    console.log(alice.calculateSalary.calls.allArgs());
+
+    expect(alice.calculateSalary.calls.allArgs()).toEqual([[4],[1000]]);
+
+    // returns objects
+    console.log(alice.calculateSalary.calls.all());
+
+    console.log(alice.calculateSalary.calls.mostRecent());
+    console.log(alice.calculateSalary.calls.first());
+
+    alice.calculateSalary.calls.reset();
+
+    expect(alice.calculateSalary.calls.any()).toEqual(false);
+
+  });
+
 });
